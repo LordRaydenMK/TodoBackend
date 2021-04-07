b5c950abf5a38e71626c9f0758f22e98a5b47efa
Stojan Anastasov <stolea@gmail.com>
2021-04-07 15:49:35 +0200

    Update CORS

--- src/main/kotlin/io/github/lordraydenmk/main.kt
+++ src/main/kotlin/io/github/lordraydenmk/main.kt
@@ -39,7 +39,7 @@ fun Application.module() {
         method(HttpMethod.Put)
         method(HttpMethod.Delete)
         method(HttpMethod.Patch)
-        allowCredentials = true
+        header(HttpHeaders.ContentType)
         anyHost()
     }
     install(ContentNegotiation) {
