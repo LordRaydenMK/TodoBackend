5c08bd5b4546c55ed6713d28a7e74a3b3c13945f
Stojan Anastasov <stolea@gmail.com>
2021-04-08 15:56:06 +0200

    LogLeve -> Debug

--- src/main/kotlin/io/github/lordraydenmk/main.kt
+++ src/main/kotlin/io/github/lordraydenmk/main.kt
@@ -24,7 +24,7 @@ fun Application.module() {
         }
     }
     install(CallLogging) {
-        level = Level.INFO
+        level = Level.DEBUG
     }
     install(CORS) {
         method(HttpMethod.Options)
