10b44e38e34c5709667b1e12ac98528f02148b12
Stojan Anastasov <stolea@gmail.com>
2021-04-08 14:45:23 +0200

    Remove default TODO

--- src/main/kotlin/io/github/lordraydenmk/main.kt
+++ src/main/kotlin/io/github/lordraydenmk/main.kt
@@ -1,8 +1,6 @@
 package io.github.lordraydenmk
 
 import io.github.lordraydenmk.data.TodoInMemoryRepository
-import io.github.lordraydenmk.domain.TodoId
-import io.github.lordraydenmk.domain.TodoItem
 import io.github.lordraydenmk.http.routes
 import io.ktor.application.*
 import io.ktor.features.*
@@ -10,16 +8,13 @@ import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.serialization.*
-import kotlinx.coroutines.runBlocking
 import org.slf4j.event.Level
-import java.util.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Used in application.conf
 fun Application.module() {
     val repo = TodoInMemoryRepository()
-    runBlocking { repo.createTodo(TodoItem(TodoId(UUID.randomUUID()), "Deploy to Heroku", false, 0)) }
     install(StatusPages) {
         status(HttpStatusCode.NotFound) {
             call.respond(HttpStatusCode.NotFound, "Not Found!")
