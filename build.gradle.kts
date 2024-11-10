83dfc0aae567c241a00fcf7a545ac1dc005e23c5
Stojan Anastasov <stolea@gmail.com>
2024-11-10 12:34:38 +0100

    Kotlin 2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     application
     id("com.github.johnrengelman.shadow") version "7.1.2"
-    kotlin("jvm") version "1.8.20"
-    kotlin("plugin.serialization") version "1.8.20"
+    kotlin("jvm") version "2.0.20"
+    kotlin("plugin.serialization") version "2.0.20"
 }
 
 group = "io.github.lordraydenmk"
